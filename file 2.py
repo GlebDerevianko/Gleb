b427abdc898d04c36a25e522a721def211ece34d
yehor <glebushe@gmail.com>
2022-05-10 12:39:13 -0400

    first commit

---
+++ file 2.py
@@ -0,0 +1,18 @@
+class Student:
+    '''  height =160
+    def __init__(self):
+        print(self.height)
+        self.height+=10'''
+
+    def __init__(self):
+        self.height = 170
+        height =160
+
+    def printer(self):
+        print(self.height)
+
+
+nick = Student()
+kate = Student()
+nick.printer()
+kate.printer()
